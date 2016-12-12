dd6d1ddf9433b1034e6e36f123697b66204abafa
bangni <bangni@weixinzhuyi.com>
2016-12-12 19:14:46 +0800

    no message

---
+++ LeanClondDemo/LeanClondDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LeanClondDemo
+//
+//  Created by 从来 on 16/12/12.
+//  Copyright © 2016年 从来. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
